5f18186c721e10bd8df2bf86e2df3db913e6b146
Trio Nugroho <ntrio23331@gmail.com>
2022-09-23 00:37:19 +0700

    styling form cars and add some dom to disabled

--- public/scripts/car.example.js
+++ public/scripts/car.example.js
@@ -39,23 +39,20 @@ class Car {
     this.specs = specs;
     this.availableAt = availableAt;
   }
-  
 
   render() {
     
     return `
         <div class="card mb-3">
-          <img src="${this.image}" class="card-img-top" height="400" alt="${this.manufacture}">
+          <img src="${this.image}" class="card-img-top" height="350" width="300" alt="${this.manufacture}">
           <div class="card-body">
-            <p class="card-text">${this.model}</p>
-            <h5 class="card-title">Rp.${this.rentPerDay} / Hari</h5>
-            <p class="card-text">${this.description}</p>
-            <p class="card-text">${this.capacity}</p>
-            <p class="card-text">${this.transmission}</p>
-            <p class="card-text">${this.year}</p>
-            <p class="card-text">${this.available}</p>
-            <p class="card-text">${this.availableAt}</p>
-            <a href="#" class="btn btn-primary">Pilih Mobil</a>
+            <p class="card-text text-js mb-2">${this.type} ${this.model}</p>
+            <h5 class="card-title text-js mb-2">Rp${this.rentPerDay.toLocaleString().replaceAll(',', '.')} / Hari</h5>
+            <p class="card-text text-js mb-2">${this.description}</p>
+            <p class="card-text"><i class="fa-solid fa-user-group i-js"></i> ${this.capacity}</p>
+            <p class="card-text"><i class="fa-solid fa-gear i-js"></i> ${this.transmission}</p>
+            <p class="card-text"><i class="fa-regular fa-calendar i-js"></i> ${this.year}</p>
+            <a href="#" class="btn text-white btn-js mt-2" style="background: #5CB85F;">Pilih Mobil</a>
           </div>
         </div>
     `;
