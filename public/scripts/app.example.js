c2e782f013a46a3bc9e93141ae99805b4aad33ad
Trio Nugroho <ntrio23331@gmail.com>
2022-09-23 15:25:09 +0700

    fix

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -27,71 +27,32 @@ class App {
 
     // Show All Cars
     if(this.driverAvail.value == 'Pilih Sopir') {
-      // Disabled when showing all cars
-      this.driverAvail.addEventListener('click', () => {
-        this.driverAvail.addEventListener('change', () => {
-          switch(this.driverAvail.value) {
-            case 'Pilih Sopir':
-              console.log('pilih sopir');
-              this.availAt = document.querySelector('#date').disabled = true;
-              this.time = document.querySelector('#time').disabled = true;
-              this.capacity = document.querySelector('#penumpang').disabled = true;
-              break;
-            case 'Dengan Sopir' || 'Tanpa Sopir':
-              console.log('tanpa');
-            this.availAt = document.querySelector('#date').disabled = false;
-              this.time = document.querySelector('#time').disabled = false;
-              this.capacity = document.querySelector('#penumpang').disabled = false;
-              break;
-          }
-        })
-      })
-      Car.list.forEach((car) => {
+        Car.list.forEach((car) => {
         const node = document.createElement("div");
         node.className = 'col-md-3';
         node.innerHTML = car.render();
         this.carContainerElement.appendChild(node);
       })
-      // Not Show All Cars (Filtered)
-    } else {
-      this.driverAvail.addEventListener('click', () => {
-        this.driverAvail.addEventListener('change', () => {
-          switch(this.driverAvail.value) {
-            case 'Pilih Sopir':
-              console.log('pilih sopir');
-              this.availAt = document.querySelector('#date').disabled = true;
-              this.time = document.querySelector('#time').disabled = true;
-              this.capacity = document.querySelector('#penumpang').disabled = true;
-              break;
-            case 'Dengan Sopir' || 'Tanpa Sopir':
-              console.log('tanpa');
-            this.availAt = document.querySelector('#date').disabled = false;
-              this.time = document.querySelector('#time').disabled = false;
-              this.capacity = document.querySelector('#penumpang').disabled = false;
-              break;
+    }
+    Car.list.forEach((car) => {
+          let dateTime = `${this.availAt.value}T${this.time.value}`;
+          let formDate = Date.parse(dateTime);
+          let waktu = Date.parse(car.availableAt);
+          let capacity = this.capacity.value;
+          
+          // Get value by available driver
+          const driver = this.driverAvail.value == 'Dengan Sopir' ? true : false;
+          // console.log(driver);
+    
+          // Filter car
+          if (car['available'] == driver && waktu >= formDate && car['capacity'] >= capacity) {
+            // console.log('Filtered Success');
+            const node = document.createElement("div");
+            node.className = 'col-md-3';
+            node.innerHTML = car.render();
+            this.carContainerElement.appendChild(node);
           }
         })
-      })
-      Car.list.forEach((car) => {
-        let dateTime = `${this.availAt.value}T${this.time.value}`;
-        let formDate = Date.parse(dateTime);
-        let waktu = Date.parse(car.availableAt);
-        let capacity = this.capacity.value;
-        
-        // Get value by available driver
-        const driver = this.driverAvail.value == 'Dengan Sopir' ? true : false;
-        // console.log(driver);
-  
-        // Filter car
-        if (car['available'] == driver && waktu >= formDate && car['capacity'] >= capacity) {
-          // console.log('Filtered Success');
-          const node = document.createElement("div");
-          node.className = 'col-md-3';
-          node.innerHTML = car.render();
-          this.carContainerElement.appendChild(node);
-        }
-      })
-    }
   };
   
   async load() {
